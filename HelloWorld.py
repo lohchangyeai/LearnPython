b700988560da96ddcfd1ad543e20a3ad92e98e9b
Chang Ye <lohchangye@gmail.com>
2024-08-21 14:37:33 +0800

    the initial commit into repository

---
+++ HelloWorld.py
@@ -0,0 +1,40 @@
+print("Hello World!")
+
+x = 5
+y = 6
+z = x*y
+
+#this line is a sample comment
+print("z=",z)
+
+#assign single value to multiple variables
+x= y= z = 100
+print ("x=",x)
+y+=1
+print ("x=",x)
+print ("y=",y)
+print ("************")
+
+#unpack a collection
+fruits = ["orange", "banana", "coconut"]
+x,y,z=fruits
+print("x=",x,"y=",y,"z=",z)
+print("x="+x,"y="+y+"z=",z)
+
+#to output number and string in the same line
+x=5
+y='John'
+print(y,x)
+
+#function 
+def sayHello():
+    local_x = 'i am local variable x'
+    global global_x
+    global_x = "i am Global variable x"
+    print('local_x=', local_x)
+    print('Hello there!')
+
+sayHello()    
+print('global_x=', global_x)
+
+
